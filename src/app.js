3878f29b46d6aca346feb4af0237c063e6837d50
Pasindu Pramod Weerarathne <pasindu@surreytechconsulting.lk>
2026-03-09 14:31:58 +0530

    Implement ESLint and Prettier

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello, World!" });
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello, World!' });
 });
 
 export default app;
